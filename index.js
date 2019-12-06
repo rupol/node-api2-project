c5a70cae85a6da58ff8c3e7d0ac2eeabb1383ff7
Ruth Poliakon <r.poliakon@gmail.com>
2019-12-05 19:52:03 -0700

    project initialization, express dependency installed and post router setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const postRouter = require("./routers/post");
+
+const server = express();
+
+server.use("/api/posts", postRouter);
+
+server.listen(4000, () => {
+  console.log("\n*** Server Running on http://localhost:4000 ***\n");
+});
